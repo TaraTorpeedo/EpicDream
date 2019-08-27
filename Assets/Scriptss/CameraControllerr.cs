938e39984e118a5599f1598f56a5bb26a045995f
TaraTorpeedo <54583576+TaraTorpeedo@users.noreply.github.com>
2019-08-27 15:33:34 +0300

    asd

---
+++ Assets/Scriptss/CameraControllerr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerr : MonoBehaviour
+{
+    public Transform PlayerTransform; //Pelaaja jota kameraa seuraa
+
+    public float smoothing = 5f; //Kameran nopeus
+
+    Vector3 offset; //Offset pelaajasta
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Laskee offsetin
+        offset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = PlayerTransform.position + offset;
+        transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scriptss/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator animator;
+    Rigidbody rigidbody;
+    int floorMask;
+    float camLenght = 100f;
+
+    void Awake() 
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate() {
+    float h = Input.GetAxisRaw("Horizontal");
+    float v = Input.GetAxisRaw("Vertical");    
+
+    Move(h, v);
+    Turning();
+    Animating(h, v);
+    }
+    void Move(float h, float v) {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rigidbody.MovePosition(transform.position + movement);   
+    }
+
+    void Turning()
+    {
+
+    }
+
+    void Animating(float h, float v)
+    {
+
+    }
+    
+}
